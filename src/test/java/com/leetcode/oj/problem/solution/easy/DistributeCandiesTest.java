93483997bea4f5595a4cfdaccfb5139d75f07597
leeyee <seadlead@gmail.com>
2017-08-29 21:57:25 +0800

    add question description

--- src/test/java/com/leetcode/oj/problem/solution/easy/DistributeCandiesTest.java
+++ src/test/java/com/leetcode/oj/problem/solution/easy/DistributeCandiesTest.java
@@ -6,8 +6,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class DistributeCandiesTest {
     DistributeCandies dis = null;
     int[] bigCandies = null;
