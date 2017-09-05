c6979240e8643af31f95144436f1b13912ddc1b7
leeyee <seadlead@gmail.com>
2017-09-05 20:42:28 +0800

    add class comment

--- src/main/java/com/leetcode/oj/problem/solution/easy/AddStrings.java
+++ src/main/java/com/leetcode/oj/problem/solution/easy/AddStrings.java
@@ -1,6 +1,17 @@
 package com.leetcode.oj.problem.solution.easy;
 
 /**
+ * URL: <a href="https://leetcode.com/problems/add-strings/description/">Add Strings</a>
+ * <p>
+ * Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+ * <p>
+ * Note:
+ * <p>
+ * The length of both num1 and num2 is < 5100.
+ * Both num1 and num2 contains only digits 0-9.
+ * Both num1 and num2 does not contain any leading zero.
+ * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+ * <p>
  * Created by leeyee on 2017/09/05.
  */
 public class AddStrings {
