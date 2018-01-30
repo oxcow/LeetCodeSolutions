4e69038cce44e4acecbf6338a720cf0ed7392c06
William Li <william.li@activenetwork.com>
2018-01-30 12:20:06 +0800

    add description

--- src/main/java/com/leetcode/oj/problem/solution/easy/JewelsAndStones.java
+++ src/main/java/com/leetcode/oj/problem/solution/easy/JewelsAndStones.java
@@ -6,6 +6,25 @@ import org.slf4j.LoggerFactory;
 import java.util.BitSet;
 
 /**
+ * URL: <a href="https://leetcode.com/problems/jewels-and-stones/description/">Jewels and Stones</a>
+ * <p>
+ * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+ * <p>
+ * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: J = "aA", S = "aAAbbbb"
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: J = "z", S = "ZZ"
+ * Output: 0
+ * Note:
+ * <p>
+ * S and J will consist of letters and have length at most 50.
+ * The characters in J are distinct.
+ * <p>
  * Created by wli on 2018-01-30.
  */
 public class JewelsAndStones {
