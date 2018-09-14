60520282843495b95f53d61edb16844c8dea92e5
William.li <William.li@activenetwork.com>
2018-09-14 17:10:49 +0800

    resolve bulls and cows, but not fast

---
+++ src/main/java/com/leetcode/oj/problem/solution/medium/BullsAndCows.java
@@ -0,0 +1,62 @@
+package com.leetcode.oj.problem.solution.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * URL: <a href="https://leetcode.com/problems/bulls-and-cows/description/">Bulls and Cows</a>
+ * <p>
+ * You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+ * <p>
+ * Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows.
+ * <p>
+ * Please note that both secret number and friend's guess may contain duplicate digits.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: secret = "1807", guess = "7810"
+ * <p>
+ * Output: "1A3B"
+ * <p>
+ * Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+ * Example 2:
+ * <p>
+ * Input: secret = "1123", guess = "0111"
+ * <p>
+ * Output: "1A1B"
+ * <p>
+ * Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
+ * Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+ */
+public class BullsAndCows {
+    public String getHint(String secret, String guess) {
+
+        char[] secretChars = secret.toCharArray();
+        char[] guessChars = guess.toCharArray();
+
+        int bulls = 0;
+
+        List<Character> guessCows = new ArrayList<>();
+        List<Character> secretCows = new ArrayList<>();
+
+        for (int i = 0; i < secretChars.length; i++) {
+            if (secretChars[i] == guessChars[i]) {
+                bulls++;
+            } else {
+                guessCows.add(guessChars[i]);
+                secretCows.add(secretChars[i]);
+            }
+        }
+
+        int cows = 0;
+
+        for (char guessChar : guessCows) {
+            if (secretCows.contains(guessChar)) {
+                cows++;
+                secretCows.remove((Character) guessChar);
+            }
+        }
+
+        return bulls + "A" + cows + "B";
+    }
+}
---
+++ src/test/java/com/leetcode/oj/problem/solution/medium/BullsAndCowsTest.java
@@ -0,0 +1,16 @@
+package com.leetcode.oj.problem.solution.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BullsAndCowsTest {
+
+    @Test
+    public void getHint() {
+        BullsAndCows bullsAndCows = new BullsAndCows();
+        Assert.assertEquals("1A3B", bullsAndCows.getHint("1807", "7810"));
+        Assert.assertEquals("1A1B", bullsAndCows.getHint("1123", "0111"));
+        Assert.assertEquals("4A0B", bullsAndCows.getHint("1111", "1111"));
+        Assert.assertEquals("0A4B", bullsAndCows.getHint("1122", "2211"));
+    }
+}
